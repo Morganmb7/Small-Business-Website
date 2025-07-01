7a10cdd20e1c8d6c0400684681f98a825b29a3bd
Morgan Bierbrunner <mbierbrunner@gmail.com>
2025-07-01 15:25:04 -0400

    made edits and inserting images

--- CC_7.js
+++ CC_7.js
@@ -1,15 +1,14 @@
-//select elmements
+//select elements
 
 const input = document.getElementById("headLineInput");
-const button = document.getElementById("UpdateButton");
+const button = document.getElementById("updateButton"); // lowercase "u"
 const headLine = document.getElementById("cta");
 
 //update cta text when button is clicked 
 button.addEventListener("click", () => {
-   const newHeadLine = input.ariaValueMax.trim ();
+   const newHeadLine = input.value.trim(); // use .value
    if (newHeadLine !== "") {
        headLine.textContent = newHeadLine;
        input.value = ""; // Clear the input field after updating
    }
-
 });
